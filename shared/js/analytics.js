c22b3575843cdd5ea9f14f5995ae03b30585c009
François Grante <grante.francois@gmail.com>
2015-12-10 14:59:22 +0100

    Analytics endpoint change

--- shared/js/analytics.js
+++ shared/js/analytics.js
@@ -1,11 +1,9 @@
 // Counts the main actions made with the extension to know which features
 // are the most successful
 //
-// Analytics are made with StatHat
-//
 
 function eventTrack(eventName) {
-  url = "https://api.stathat.com/ez?ezkey=antoine@emailhunter.co&stat="+eventName;
+  url = "https://emailhunter.co/events?name="+eventName;
   $.ajax({
     url : url,
     type : 'GET',
