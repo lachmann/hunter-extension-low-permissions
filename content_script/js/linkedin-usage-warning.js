c4a2cd1ccff95afce3f749ad43ba36f620ef004e
François Grante <grante.francois@gmail.com>
2016-06-23 16:37:10 +0200

    Warning about LinkedIn limitation restored

--- content_script/js/linkedin-usage-warning.js
+++ content_script/js/linkedin-usage-warning.js
@@ -39,12 +39,18 @@ function updateUsageWarningViewed(value) {
   });
 }
 
+function updateUsageWarningHide(value) {
+  chrome.storage.sync.set({'usage_warning_hide': value}, function() {
+    // Never show the warning in the future
+  });
+}
+
 function incrementProfileViewsCount() {
   chrome.storage.sync.get('linkedin_profile_views', function(value){
     if (typeof value["linkedin_profile_views"] !== "undefined") {
       views_count = value["linkedin_profile_views"] + 1;
       updateProfileViewsCount(views_count);
-      //prepareLimitationWarning(views_count);
+      prepareLimitationWarning(views_count);
     }
     else {
       updateProfileViewsCount(1);
@@ -54,9 +60,13 @@ function incrementProfileViewsCount() {
 
 function prepareLimitationWarning(views_count) {
   if (views_count >= 400) {
-    chrome.storage.sync.get('usage_warning_viewed', function(value) {
-      if (typeof value["usage_warning_viewed"] === "undefined" || value["usage_warning_viewed"] == false) {
-        displayLimitationWarning();
+    chrome.storage.sync.get('usage_warning_hide', function(hide){
+      if (typeof hide['usage_warning_hide'] === "undefined") {
+        chrome.storage.sync.get('usage_warning_viewed', function(value) {
+          if (typeof value["usage_warning_viewed"] === "undefined" || value["usage_warning_viewed"] == false) {
+            displayLimitationWarning();
+          }
+        });
       }
     });
   }
@@ -64,11 +74,17 @@ function prepareLimitationWarning(views_count) {
 
 function displayLimitationWarning() {
   var logo = chrome.extension.getURL('shared/img/orange_transparent_logo.png');
-  $("body").append('<div id="eh_bar_usage_warning"><div class="eh_usage_warning_container"><img src="' + logo + '" alt="Email Hunter"><br/>Wow! It seems you have visited an important number of profiles on LinkedIn today. LinkedIn may block your access if you continue to visit pages today. Please note that each profile saved from LinkedIn\'s search with Email Hunter is also a page view.<br/><br/><span id="eh_close_bar_usage_warning">I understand the risks, thank you</span></div></div>');
+  $("body").append('<div id="eh_bar_usage_warning"><form id="eh_usage_warning_form"><img src="' + logo + '" alt="Email Hunter"><br/>Wow! It seems you have visited an important number of profiles on LinkedIn today. LinkedIn may block your access if you continue to visit pages today. Please note that each profile saved from LinkedIn\'s search with Email Hunter is also a page view.<br/><br/><label for="eh_usage_warning_dont_show"><input type="checkbox" id="eh_usage_warning_dont_show">Don\'t show me this in the future</label><button type="submit">OK, I understand the risks</button></form></div>');
 
-  $("#eh_close_bar_usage_warning").click(function() {
+  $("#eh_usage_warning_form").submit(function() {
     $("#eh_bar_usage_warning").fadeOut(200);
     updateUsageWarningViewed(true);
+
+    if ($("#eh_usage_warning_dont_show").prop("checked") == true) {
+      updateUsageWarningHide(true);
+    }
+
+    return false;
   });
 }
 
