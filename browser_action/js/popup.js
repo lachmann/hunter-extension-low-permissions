517a2e651874cf588f43814c13c1637197acfe9e
François Grante <grante.francois@gmail.com>
2016-11-15 02:34:47 +0700

    No longer chrome.tab methos which is not authorized

--- browser_action/js/popup.js
+++ browser_action/js/popup.js
@@ -5,26 +5,17 @@
 // - If it's a LinkedIn search page, allow to save leads from it
 // - In other cases, do a Domain Search
 //
-chrome.tabs.getSelected(null, function(tab) {
-  window.domain = new URL(tab.url).hostname.replace("www.", "");
 
-  if (window.domain == "linkedin.com") {
-    chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
-      chrome.tabs.sendMessage(tabs[0].id, {subject: "linkedin_page_type"}, function(response) {
-        if (typeof response != "undefined" && response.linkedin_page_type == "profile") {
-          LinkedinProfile.launch();
-        }
-        else if (typeof response != "undefined" && response.linkedin_page_type == "search")  {
-          LinkedinSearch.launch();
-        }
-        else {
-          $("#domain-search").show();
-        }
-      });
-    });
-  }
-  else {
-    $("#domain-search").show();
-    DomainSearch.launch();
-  }
+chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
+  chrome.tabs.sendMessage(tabs[0].id, {subject: "linkedin_page_type"}, function(response) {
+    if (typeof response != "undefined" && response.linkedin_page_type == "profile") {
+      LinkedinProfile.launch();
+    }
+    else if (typeof response != "undefined" && response.linkedin_page_type == "search")  {
+      LinkedinSearch.launch();
+    }
+    else {
+      $("#domain-search").show();
+    }
+  });
 });
